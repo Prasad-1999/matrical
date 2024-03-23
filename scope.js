cc7bace419a477faaec9302e8b87705a2027b32c
Prasad-1999 <92925789+Prasad-1999@users.noreply.github.com>
2024-03-23 09:32:17 +0530

    init

---
+++ scope.js
@@ -0,0 +1,18 @@
+// function object(){
+//     console.log(object1)
+// }
+// var object1 = {
+//     name:'RRR',
+//     director:'rajamouli'
+// }
+// object()
+
+// var s1= [1,2,3,4]
+// var s2 = new Set(s1)
+// console.log(s1)
+// console.log(s2)
+
+// var a = 'hello'
+// for (var i=0;i<=10;i++){
+//     console.log(a)
+// }
